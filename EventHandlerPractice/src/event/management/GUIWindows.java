9e2c5a52cf83497e29c5c76f41f2b4fc7edda388
chrisng16 <chrisng16@users.noreply.github.com>
2020-07-30 22:25:02 -0700

    minor change

--- EventHandlerPractice/src/event/management/GUIWindows.java
+++ EventHandlerPractice/src/event/management/GUIWindows.java
@@ -4,7 +4,6 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class GUIWindows {
-
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
